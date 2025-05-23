32562c816d705f97927e5632bd413940a092bd68
Abdulmjeed <abdoksa1122@hotmail.com>
2025-05-23 16:36:08 +0300

    Add project files..

---
+++ DSAR/ViewModels/UserView.cs
@@ -0,0 +1,31 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+﻿using DSAR.Models;
+
+namespace DSAR.ViewModels
+{
+    public class UserView
+    {
+        
+        public string FullName { get; set; }
+        public List<User> Users { get; set; }
+        public string Email { get; set; }
+
+    }
+    
+=======
+=======
+>>>>>>> 56832847cdeabe744161f1af819174b8cf5fe246
+﻿namespace DSAR.ViewModels
+{
+    public class UserView
+    {
+        public int Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+<<<<<<< HEAD
+>>>>>>> 56832847cdeabe744161f1af819174b8cf5fe246
+=======
+>>>>>>> 56832847cdeabe744161f1af819174b8cf5fe246
+}
